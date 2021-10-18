a75df80b9b9ee59ae1d6be0a3f2f63b35136563d
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2021-10-18 15:02:07 +0200

    First test scenario completed

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,19 @@
+import io.restassured.RestAssured;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void helloWrold() {
+        System.out.println("Hello world");
+
+        RestAssured
+                .given()
+                    .baseUri("http://numbersapi.com")
+                    .log().all()
+                .when()
+                    .get("/1410/year")
+                .then()
+                    .log().all();
+    }
+}
