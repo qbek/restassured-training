c5be6a0f650b821b9c86b1826217a08254c7e76b
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2022-09-06 11:42:21 +0200

    Refaktor: definicja bazowego szablonu dla requestow

--- src/test/java/ProjectCreationTest.java
+++ src/test/java/ProjectCreationTest.java
@@ -1,6 +1,11 @@
+import io.cucumber.java.Before;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
@@ -9,20 +14,28 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class ProjectCreationTest {
 
+
+
+    @BeforeAll
+    public static void setup() {
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        RestAssured.requestSpecification = builder
+                .setBaseUri("https://api.todoist.com")
+                .setBasePath("/rest/v1")
+                .addHeader("Authorization", "Bearer d469ce54eca3a7ca5b6b5e7d4c8d51ced8d4c7b1")
+                .setContentType(ContentType.JSON)
+                .build();
+    }
     @Test
     public void userCanCreateAProject() {
         long projectId = RestAssured
                 .given()
-                    .baseUri("https://api.todoist.com")
-                    .basePath("/rest/v1")
-                    .header("Authorization", "Bearer d469ce54eca3a7ca5b6b5e7d4c8d51ced8d4c7b1")
-                    .contentType(ContentType.JSON)
                     .body("{\"name\": \"Szkolenie RestAssured\"}")
-                    .log().all()
                 .when()
                     .post("/projects")
                 .then()
-                    .log().all()
                     .assertThat()
                         .statusCode(200)
                         .body("name", equalTo("Szkolenie RestAssured"))
@@ -31,36 +44,23 @@ public class ProjectCreationTest {
 
         RestAssured
                 .given()
-                    .baseUri("https://api.todoist.com")
-                    .basePath("/rest/v1")
                     .pathParam("id", projectId)
-                    .header("Authorization", "Bearer d469ce54eca3a7ca5b6b5e7d4c8d51ced8d4c7b1")
-                    .log().all()
                 .when()
                     .get("/projects/{id}")
                 .then()
-                    .log().all()
                     .assertThat()
                         .statusCode(200)
                         .body("id", equalTo(projectId))
                         .body("name", equalTo("Szkolenie RestAssured"));
 
-
         RestAssured
                 .given()
-                    .baseUri("https://api.todoist.com")
-                    .basePath("/rest/v1")
-                    .header("Authorization", "Bearer d469ce54eca3a7ca5b6b5e7d4c8d51ced8d4c7b1")
-                    .log().all()
                 .when()
                     .get("/projects")
                 .then()
-                    .log().all()
                     .assertThat()
                         .statusCode(200)
                         .body(format("find{ it.id == %d }.name", projectId), equalTo("Szkolenie RestAssured"));
-
-
 
     }
 
