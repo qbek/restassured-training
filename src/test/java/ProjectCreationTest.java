0acb9b21e006378effbf76467c12d27c23420b35
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2022-09-07 10:02:30 +0200

    Dokonczenie refaktoru + test drugi

--- src/test/java/ProjectCreationTest.java
+++ src/test/java/ProjectCreationTest.java
@@ -14,6 +14,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class ProjectCreationTest {
 
+
     @BeforeAll
     public static void setup() {
         RequestSpecBuilder builder = new RequestSpecBuilder();
@@ -26,12 +27,63 @@ public class ProjectCreationTest {
                 .setContentType(ContentType.JSON)
                 .build();
     }
+
     @Test
     public void userCanCreateAProject() {
         String projectName = "Szkolenie RestAssured ze zmiennej";
         long projectId = createNewProject(projectName);
         checkIfProjectIsCreated(projectId, projectName);
         checkIfProjectIsOnAllProjectList(projectId, projectName);
+    }
+
+    @Test
+    public void userCanAddTaksToTheProject() {
+        String projectName = "Projekt z zadaniem";
+        String taskName = "to jest moje zadanie";
+        long projectId = createNewProject(projectName);
+        long taskId = addTaskToTheProject(taskName, projectId);
+        checkIfTaskIsCreated(taskId, taskName);
+        checkIfTaskIsOnAllTasksList(taskId, taskName);
+    }
+
+    private void checkIfTaskIsOnAllTasksList(long taskId, String taskName) {
+        RestAssured
+                .given()
+                .when()
+                    .get("/tasks")
+                .then()
+                    .assertThat()
+                        .statusCode(200)
+                        .body(format("find{ it.id == %d }.content", taskId), equalTo(taskName));
+    }
+
+    private void checkIfTaskIsCreated(long taskId, String taskName) {
+        RestAssured
+                .given()
+                    .pathParam("id", taskId)
+                .when()
+                    .get("/tasks/{id}")
+                .then()
+                    .assertThat()
+                        .statusCode(200)
+                        .body("content", equalTo(taskName));
+    }
+
+    private long addTaskToTheProject(String taskName, long projectId) {
+        return RestAssured
+                .given()
+                    .body(
+                            format("{ \"content\": \"%s\", \"project_id\": %d}", taskName, projectId)
+                    )
+                .when()
+                    .post("/tasks")
+                .then()
+                    .assertThat()
+                        .statusCode(200)
+                        .body("content", equalTo(taskName))
+                        .body("project_id", equalTo(projectId))
+                    .and()
+                        .extract().path("id");
     }
 
     private void checkIfProjectIsOnAllProjectList(long projectId, String projectName) {
@@ -72,6 +124,8 @@ public class ProjectCreationTest {
                         .extract().path("id");
         return projectId;
     }
+
+
 }
 
 
