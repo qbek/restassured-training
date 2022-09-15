3e078c9bc645ef19da77d463b067865b156869b5
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2022-09-15 12:46:52 +0200

    Zmienne srodowiskowe

--- src/test/java/org/example/BaseSetup.java
+++ src/test/java/org/example/BaseSetup.java
@@ -6,17 +6,27 @@ import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @ExtendWith(SerenityJUnit5Extension.class)
 public abstract class BaseSetup {
 
     @BeforeAll
-    public static void setup() {
+    public static void setup() throws IOException {
         RestAssured.reset();
         RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+
+        String filename = String.format("%s.properties", System.getProperty("env"));
+        InputStream file = reqBuilder.getClass().getResourceAsStream("/" + filename);
+        Properties cfg = new Properties();
+        cfg.load(file);
+
         RestAssured.requestSpecification = reqBuilder
-                .setBaseUri("https://api.todoist.com")
-                .setBasePath("/rest/v1")
-                .addHeader("Authorization", "Bearer d469ce54eca3a7ca5b6b5e7d4c8d51ced8d4c7b1")
+                .setBaseUri(cfg.getProperty("baseUri"))
+                .setBasePath(cfg.getProperty("basePath"))
+                .addHeader("Authorization", "Bearer " + cfg.getProperty("token"))
                 .build();
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
