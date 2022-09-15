63b241d3717524963ff58b56e86c5b5e1a1a4c1c
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2022-09-15 09:12:05 +0200

    Fix na zwielokrotniony naglowek w kolejnych test suitach

--- src/test/java/org/example/BaseSetup.java
+++ src/test/java/org/example/BaseSetup.java
@@ -8,14 +8,13 @@ public abstract class BaseSetup {
 
     @BeforeAll
     public static void setup() {
+        RestAssured.reset();
         RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
-
         RestAssured.requestSpecification = reqBuilder
                 .setBaseUri("https://api.todoist.com")
                 .setBasePath("/rest/v1")
                 .addHeader("Authorization", "Bearer d469ce54eca3a7ca5b6b5e7d4c8d51ced8d4c7b1")
                 .build();
-
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
 }
