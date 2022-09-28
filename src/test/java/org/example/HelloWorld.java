9eb4569eb89500ea36e52d9708bc3d392bb49a6d
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2022-09-28 15:31:28 +0200

    First test

---
+++ src/test/java/org/example/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorld {
+
+    @Test
+    public void helloWorld() {
+        RestAssured
+                .given()
+                    .log().all()
+                .when().get("http://google.com")
+                .then()
+                .log().all();
+    }
+}
