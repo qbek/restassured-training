76db8eb4247a63bd989be62294863aad16685d5c
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2023-11-22 15:48:09 +0100

    Final first version of scenario

--- src/test/java/org/example/ProjectTests.java
+++ src/test/java/org/example/ProjectTests.java
@@ -12,7 +12,7 @@ public class ProjectTests {
 
     @Test
     public void userCanCreateANewProject() {
-        RestAssured
+        var projectId = RestAssured
                 .given()
                     .baseUri("https://api.todoist.com")
                     .basePath("/rest/v2")
@@ -27,21 +27,32 @@ public class ProjectTests {
                     .assertThat()
                         .statusCode(200)
                         .header("Content-Type", Matchers.containsString("json"))
-                        .body("name", Matchers.equalTo("Shopping List"));
+                        .body("name", Matchers.equalTo("Shopping List"))
+                    .and()
+                        .extract().path("id");
 
         RestAssured
                 .given()
                     .baseUri("https://api.todoist.com")
                     .basePath("/rest/v2")
                     .header("Authorization", "Bearer d469ce54eca3a7ca5b6b5e7d4c8d51ced8d4c7b1")
+                    .pathParam("id", projectId)
                     .log().all()
                 .when()
-                    .get("/projects/2324023681")
+                    //konkatenacja stringow
+//                    .get("/projects/" + projectId)
+                    // szablony tekstu
+//                    .get(String.format("/projects/%s", projectId))
+                    .get("/projects/{id}")
                 .then()
                     .log().all()
                     .assertThat()
                         .statusCode(200)
+                        .body("id", Matchers.equalTo(projectId))
                         .body("name", Matchers.equalTo("Shopping List"));
+
+        var getNameByProjectId = String.format("find{ it.id == \"%s\" }.name", projectId );
+        var getProjectByProjectId = String.format("find{ it.id == \"%s\" }", projectId );
 
         RestAssured
                 .given()
@@ -54,8 +65,11 @@ public class ProjectTests {
                 .then()
                     .log().all()
                     .assertThat()
-                        .statusCode(200);
+                        .statusCode(200)
+                        .body(getProjectByProjectId, Matchers.notNullValue())
+                        .body(getNameByProjectId, Matchers.equalTo("Shopping List"));
     }
 
-
 }
+
+
